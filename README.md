9dfc30c01366c479599a39b2e2c1ff0d99d3fb7e
manoj_s <apple@manojsatwase.local>
2025-03-04 07:24:03 +0530

    install daisy ui and  navbar

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 - Created a Vite + React Application
 - Remove Unnecessary code and create a Hello World app
 - Install Tailwind css
+- Install Daisy UI
+- Add Navbar component to APP.jsx
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <div>
-      
+     <Navbar />
+     <h2>Hello World</h2>
     </div>
   )
 }
