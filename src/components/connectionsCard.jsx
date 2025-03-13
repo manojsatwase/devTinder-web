9c15f81fe22b14bf8bbffed9a6fbaaaaebfb32b0
manoj_s <apple@manojsatwase.local>
2025-03-14 03:53:56 +0530

    refactor code

--- src/components/connectionsCard.jsx
+++ src/components/connectionsCard.jsx
@@ -1,28 +1,32 @@
+import { Link } from "react-router-dom";
 import { capitalFirstLetter } from "../utils/constants";
 
 const connectionsCard = ({ connection }) => {
-    const { _id,firstName, lastName, photoUrl, age, gender, about ,skills} = connection;
+    const { _id, firstName, lastName, photoUrl, age, gender, about } =
+          connection;
     return (
-        <div  className="m-4 p-4 text-left rounded-lg w-75 bg-base-300">
-            <img
+        <div
+            key={_id}
+            className="flex m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto justify-between"
+          >
+            <div className="flex flex-wrap">
+              <img
                 alt="photo"
+                className="w-20 h-20 rounded-full object-cover"
                 src={photoUrl}
-                className="w-20 h-20 rounded-full"
-            />
-            <h2 className="mt-2">{`${capitalFirstLetter(firstName)} ${capitalFirstLetter(lastName)}`}</h2>
-            <div className="flex justify-between">
-              <p className="my-1">Age : {age}</p>
-              <p>Gender : {capitalFirstLetter(gender)}</p>
+              />
+             <div className="text-left mx-10">
+              <h2 className="font-bold text-xl">
+                {capitalFirstLetter(firstName) + " " + capitalFirstLetter(lastName)}
+              </h2>
+              {age && gender && <p>{age + ", " + capitalFirstLetter(gender)}</p>}
+              <p>{about}</p>
             </div>
-            <h2 className="text-pink-500">Skills:</h2>
-            <ul className="flex flex-wrap">
-            {skills.map(skill=>(
-               <li className="mx-1" key={skill}>• {skill}</li>
-            ))}
-            </ul>
-            <h2 className="mt-2 text-cyan-500">About :</h2>
-            <p>{about}</p>
-        </div>
+            </div>
+            <Link to={"/chat/" + _id}>
+              <button className="btn btn-primary bg-pink-500 border-0 px-4 py-2 mt-5">Chat</button>
+            </Link>
+          </div>
     )
 }
 
