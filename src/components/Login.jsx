135c9796d3a7a63763f22e39dbda15ed09451c6a
manoj_s <apple@manojsatwase.local>
2025-03-13 16:24:11 +0530

    login & signup functionality

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,84 +4,129 @@ import { useDispatch, useSelector } from "react-redux";
 import { addUser } from "../utils/userSlice";
 import { useNavigate } from "react-router-dom";
 import { BASE_URL } from "../utils/constants";
-
 const Login = () => {
-  const [emailId, setEmailId] = useState("manoj@gmail.com");
-  const [password, setPassword] = useState("Manoj@123");
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [isLoginForm, setIsLoginForm] = useState(true);
   const [error, setError] = useState("");
-
   const dispatch = useDispatch();
   const navigate = useNavigate();
-
-  const userData = useSelector((store) => store.user);
-
-  // If user is already logged in, redirect to home
-  useEffect(() => {
-    if (userData) {
-      navigate("/"); // Navigation should happen after render
-    }
-  }, [userData, navigate]);
 
   const handleLogin = async () => {
     try {
       const res = await axios.post(
-        `${BASE_URL}/login`,
-        { emailId, password },
+        BASE_URL + "/login",
+        {
+          emailId,
+          password,
+        },
         { withCredentials: true }
       );
-      const { message, user } = res?.data;
-      // Dispatch the user data to Redux store
-      dispatch(addUser(user));
-      navigate("/"); // Navigate to home after successful login
+      dispatch(addUser(res.data));
+      return navigate("/");
     } catch (err) {
-      setError(err?.response?.data || "Something went wrong!");
+      setError(err?.response?.data || "Something went wrong");
+    }
+  };
+
+  const handleSignUp = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/signup",
+        { firstName, lastName, emailId, password },
+        { withCredentials: true }
+      );
+      dispatch(addUser(res.data.data));
+      return navigate("/profile");
+    } catch (err) {
+      setError(err?.response?.data || "Something went wrong");
     }
   };
 
   return (
     <div className="flex justify-center my-10">
-      <div className="card card-border bg-base-300 w-96">
+      <div className="card bg-base-300 w-96 shadow-xl">
         <div className="card-body">
-          <h2 className="card-title justify-center">LogIn</h2>
+          <h2 className="card-title justify-center">
+            {isLoginForm ? "Login" : "Sign Up"}
+          </h2>
           <div>
-            <label className="form-control w-full max-w-xs">
+            {!isLoginForm && (
+              <>
+                <label className="form-control w-full max-w-xs my-2">
+                  <div className="label">
+                    <span className="label-text">First Name</span>
+                  </div>
+                  <input
+                    type="text"
+                    value={firstName}
+                    placeholder="Enter the first name"
+                    className="input input-bordered py-2 my-2 w-full max-w-xs"
+                    onChange={(e) => setFirstName(e.target.value)}
+                  />
+                </label>
+                <label className="form-control w-full max-w-xs my-2">
+                  <div className="label">
+                    <span className="label-text">Last Name</span>
+                  </div>
+                  <input
+                    type="text"
+                    value={lastName}
+                    placeholder="Enter the last name"
+                    className="input input-bordered py-2 my-2 w-full max-w-xs"
+                    onChange={(e) => setLastName(e.target.value)}
+                  />
+                </label>
+              </>
+            )}
+            <label className="form-control w-full max-w-xs my-2">
               <div className="label">
-                <span className="label-text">Email</span>
+                <span className="label-text">Email ID:</span>
               </div>
               <input
                 type="text"
                 value={emailId}
-                placeholder="Please Enter Valid Email ID"
-                className="input input-bordered w-full max-w-xs py-2 border-0 my-2"
+                placeholder="Enter the email address"
+                className="input input-bordered py-2 my-2 w-full max-w-xs"
                 onChange={(e) => setEmailId(e.target.value)}
               />
             </label>
-            <label className="form-control w-full max-w-xs">
+            <label className="form-control w-full max-w-xs my-2">
               <div className="label">
                 <span className="label-text">Password</span>
               </div>
               <input
                 type="password"
                 value={password}
-                placeholder="Please Enter Valid Password"
-                className="input input-bordered w-full max-w-xs py-2 border-0 my-2"
+                placeholder="Enter the password"
+                className="input input-bordered py-2 my-2 w-full max-w-xs"
                 onChange={(e) => setPassword(e.target.value)}
               />
             </label>
           </div>
           <p className="text-red-500">{error}</p>
-          <div className="card-actions justify-end">
+          <div className="card-actions justify-center">
             <button
-              className="btn btn-primary bg-primary border-0 p-2"
-              onClick={handleLogin}
+              className="btn btn-primary bg-blue-500 border-0 px-4 py-2"
+              onClick={isLoginForm ? handleLogin : handleSignUp}
             >
-              Login
+              {isLoginForm ? "Login" : "Sign Up"}
             </button>
           </div>
+
+          <p
+            className="m-auto cursor-pointer py-2"
+            onClick={() => setIsLoginForm((value) => !value)}
+          >
+            {isLoginForm
+              ? "New User? Signup Here"
+              : "Existing User? Login Here"}
+          </p>
         </div>
       </div>
     </div>
   );
 };
-
 export default Login;
