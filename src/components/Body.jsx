c23edd0a3686b7d3ef6a41fdc4b3d8546b871683
manoj_s <apple@manojsatwase.local>
2025-03-13 16:27:56 +0530

    format extra line

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,19 +1,17 @@
+import axios from 'axios';
 import React, {  useEffect } from 'react'
 import { Outlet, useNavigate } from 'react-router-dom';
 
-import Footer from './Footer';
-
-import axios from 'axios';
 import { BASE_URL } from '../utils/constants';
 import { useDispatch, useSelector } from 'react-redux';
 import { addUser } from '../utils/userSlice';
 import NavBar from './NavBar';
+import Footer from './Footer';
 
 const Body = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const userData = useSelector(store=>store.user);
-
+  const userData = useSelector(store=>store?.user);
 
   const fetchUser = async () => {
     if(userData) return;
